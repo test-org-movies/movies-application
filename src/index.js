d97314247d829f993f85f88594bbfdcdef7871b8
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-28 17:00:36 -0500

    adding movies functionality working

--- src/index.js
+++ src/index.js
@@ -2,23 +2,51 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const {addMovie} = require('./api.js');
 
 getMovies().then((movies) => {
-  // console.log('Here are all the AWESOME movies:');
-  movies.forEach(({title, rating}) => {
-    return $('#movieList').append
-    (`<ul> 
+    renderMovies(movies)
+}).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+});
+
+function renderMovies(movies) {
+    movies.forEach(({title, rating}) => {
+        return $('#movieList').append
+        (`<ul> 
     <li>${title} - rating: ${rating}</li>
     </ul>`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    });
+}
+
+//movie post
+
+function newMovieObject(title, rating, genre) {
+  let userMovieTitle = $('.title').val();
+  let userMovieStars = $('.stars').val();
+  let userMovieGenre = $('.genre').val();
+
+    const newMovie = {
+        'title': userMovieTitle,
+        'rating': userMovieStars,
+        'genre': userMovieGenre
+    };
+    return newMovie;
+}
+
+$('.addMovieBtn').click(function (e) {
+    e.preventDefault();
+    addMovie(newMovieObject());
+    getMovies().then((movies) => {
+      renderMovies(movies);
+    });
 });
 
