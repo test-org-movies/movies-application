f8f9480301392df4bc64a298ad4e8e60cbcda93d
Julia Contreras <juliacontreras483@gmail.com>
2020-05-28 10:15:34 -0500

    adding forms/styling

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  console.log('Here are all the AWESOME movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
@@ -18,3 +18,4 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
